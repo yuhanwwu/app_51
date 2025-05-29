ee9af039163d8f20089f27fe4443de7e0f547c5d
yuhan <yw9223@ic.ac.uk>
2025-05-29 14:48:31 +0100

    fix(frontend): add dummy test

--- frontend/test/widget_test.dart
+++ frontend/test/widget_test.dart
@@ -11,6 +11,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:frontend/main.dart';
 
 void main() {
+  test('dummy test', () {
+    expect(5, 5);
+  });
   // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
   //   // Build our app and trigger a frame.
   //   await tester.pumpWidget(MyApp());

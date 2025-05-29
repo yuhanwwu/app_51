2425236e4c32ba58de12e19920e7d2ddfa500561
xi-tingg <xi-tingg@users.noreply.github.com>
2025-05-29 21:23:47 +0000

    deploy: 53a046405a02d783ce1fe44e3556d047bb8d260e

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2858227476"
+    serviceWorkerVersion: "605582211"
   }
 });

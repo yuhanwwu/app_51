ed28e7cf04e4b37c030169860ee867c0e1fbbddd
lewenoh <lewenoh@users.noreply.github.com>
2025-06-05 16:30:13 +0000

    deploy: 27e6c7518446a0ae18108a4dc7f0dc7d51d92b3e

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"109150893958777c8f2215f6cfd3e89e984e8d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4047882580"
+    serviceWorkerVersion: "2303762768"
   }
 });
